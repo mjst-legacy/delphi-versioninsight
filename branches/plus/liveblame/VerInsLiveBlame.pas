344449944c4d6c90c4c066bd3bfe0c87ee6e9b27
uschuster <uschuster@6172efa2-bc72-412b-a7f0-84a214907881>
2011-07-27 23:49:27 +0000

    - Merged /branches/experimental/liveblame @86 - update License150.rtf

--- branches/plus/liveblame/VerInsLiveBlame.pas
+++ branches/plus/liveblame/VerInsLiveBlame.pas
@@ -2145,6 +2145,7 @@ begin
           FLiveBlameData.FPaintBox := FPaintBox;
         end;
         FSpeedButton.Down := FLiveBlameData.FButtonDown;
+        TAction(FSpeedButton.Action).Checked := FLiveBlameData.FButtonDown;
         ModuleChanged := True;
       end;
     end;

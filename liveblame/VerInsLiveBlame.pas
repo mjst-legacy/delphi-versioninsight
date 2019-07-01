5dedfdbf168ed6952b075087cbfa07b5d6e74ee2
Michael J. Stallinger <at.mjst@mjst.at>
2019-07-01 17:07:30 +0200

    Add Delphi 10.3 (Rio) support

--- liveblame/VerInsLiveBlame.pas
+++ liveblame/VerInsLiveBlame.pas
@@ -3676,6 +3676,9 @@ const
   {$IFDEF VER320}
   coreide = 'coreide250.bpl';
   {$ENDIF VER320}
+  {$IFDEF VER330}
+  coreide = 'coreide260.bpl';
+  {$ENDIF VER330}
   SLineIsElidedName = '@Editorcontrol@TCustomEditControl@LineIsElided$qqri';
 
 function LineIsElided(Self: TObject; LineNum: Integer): Boolean; external coreide name SLineIsElidedName;
